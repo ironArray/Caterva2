56c912f3ce043a60d9e1c099bfe20a7bc32b8735
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-12-20 13:07:45 +0100

    Add code to start with empty state directory in tests
    Disabled by default.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import signal
 import subprocess
 import time
@@ -12,13 +13,17 @@ from pathlib import Path
 @pytest.fixture(scope='session')
 def services():
     tests_dir = Path('tests')
+    purge_var = False  # toggle to start with an empty state directory
 
     data_dir = tests_dir / 'data'
     if not data_dir.is_dir() and not data_dir.is_symlink():
         data_dir.symlink_to('../root-example', target_is_directory=True)
 
     var_dir = tests_dir / 'var'
-    (var_dir / 'supervisord-logs').mkdir(exist_ok=True)
+    if purge_var and var_dir.is_dir():
+        shutil.rmtree(var_dir)
+    var_dir.mkdir(exist_ok=not purge_var)
+    (var_dir / 'supervisord-logs').mkdir(exist_ok=not purge_var)
 
     popen = subprocess.Popen(['supervisord', '-c', tests_dir / 'supervisor.conf'])
     assert popen.wait() == 0
