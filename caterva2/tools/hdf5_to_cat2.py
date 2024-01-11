a84f5b3e07817bd21e845c478713afe6ff3e93f2
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-01-11 10:13:32 +0100

    hdf5_to_cat2: Add some docstrings

--- caterva2/tools/hdf5_to_cat2.py
+++ caterva2/tools/hdf5_to_cat2.py
@@ -17,16 +17,19 @@ import h5py
 
 
 def node_exporter(c2_root):
+    """Return an HDF5 node item visitor to export to existing Caterva2 root at `c2_root`."""
     def export_node(name, node):
         print(f"Export {type(node).__name__} {name!r} -> {str(c2_root)!r}")
     return export_node
 
 
 def export_group(h5_group, c2_root):
+    """Export open HDF5 group `h5_group` to existing Caterva2 root at `c2_root`."""
     h5_group.visititems(node_exporter(c2_root))
 
 
 def export(hdf5_path, cat2_path):
+    """Export HDF5 file in `hdf5_path` to new Caterva2 root at `cat2_path`."""
     with h5py.File(hdf5_path, 'r') as h5f:
         c2r = pathlib.Path(cat2_path).resolve()
         c2r.mkdir(parents=True)
