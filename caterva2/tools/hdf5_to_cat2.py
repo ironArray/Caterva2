eaf7aa8b6e0601efacd13b7d3036d15f30af538d
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-01-11 09:55:37 +0100

    hdf5_to_cat2: Open source & destination arguments
    Accessing the HDF5 file depends on h5py, added tool dependency.

--- caterva2/tools/hdf5_to_cat2.py
+++ caterva2/tools/hdf5_to_cat2.py
@@ -10,7 +10,16 @@
 """Export datasets in an HDF5 file to a Caterva2 root."""
 
 import os
+import pathlib
 import sys
+
+import h5py
+
+
+def export(hdf5_path, cat2_path):
+    with h5py.File(hdf5_path, 'r') as h5f:
+        c2r = pathlib.Path(cat2_path).resolve()
+        c2r.mkdir(parents=True)
 
 
 def main():
@@ -20,6 +29,8 @@ def main():
         prog = os.path.basename(sys.argv[0])
         print(f"Usage: {prog} HDF5_FILE CATERVA2_ROOT", file=sys.stderr)
         sys.exit(1)
+
+    export(hdf5_path, cat2_path)
 
 
 if __name__ == '__main__':
