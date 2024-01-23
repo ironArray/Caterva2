ef78bb5435d6d1404f2eb692dc72cfce21c458f8
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-01-23 14:20:20 +0100

    Move test execution function into tests module, as script
    It can be executed with `python -m caterva2.tests`.

---
+++ caterva2/tests/__main__.py
@@ -0,0 +1,25 @@
+import pathlib
+
+import pytest
+
+
+def main(verbose=False):
+    """Run the test suite.
+
+    Parameters
+    ----------
+    verbose : bool
+        If True, run the tests in verbose mode.
+
+    Returns
+    -------
+    int
+        Exit code of the test suite.
+    """
+    test_dir = pathlib.Path(__file__).parent
+    verb = "-v" if verbose else ""
+    return pytest.main([verb, test_dir])
+
+
+if __name__ == '__main__':
+    main()
