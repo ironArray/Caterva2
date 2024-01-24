6c6ee3a81d19b46fc2f9da7a188b1541bc210a93
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-01-24 12:21:01 +0100

    Remove non-functional update of test main function wrapper

--- caterva2/tests/__init__.py
+++ caterva2/tests/__init__.py
@@ -1,8 +1,6 @@
 def main(*args, **kwargs):
-    import functools
+    """An alias for `caterva2.tests.__main__.main()`."""
     # Avoid `RuntimeWarning` about import order of ``__main__``
     # resulting in unpredictable behaviour.
     from caterva2.tests.__main__ import main as tests_main
-
-    functools.update_wrapper(main, tests_main)
     return tests_main(*args, **kwargs)
