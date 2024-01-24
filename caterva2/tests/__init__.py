d3dae45f7ce5539c8df979ca78f7550113e165c2
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-01-24 10:35:32 +0100

    Avoid RuntimeWarning about import order of caterva2.tests.__main__

--- caterva2/tests/__init__.py
+++ caterva2/tests/__init__.py
@@ -1,1 +1,8 @@
-from caterva2.tests.__main__ import main
+def main(*args, **kwargs):
+    import functools
+    # Avoid `RuntimeWarning` about import order of ``__main__``
+    # resulting in unpredictable behaviour.
+    from caterva2.tests.__main__ import main as tests_main
+
+    functools.update_wrapper(main, tests_main)
+    return tests_main(*args, **kwargs)
