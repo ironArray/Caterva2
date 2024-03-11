be583d68c56d9ca91a0df59ee82d07d908802671
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-03-11 11:53:37 +0100

    Add missing copyright node to directory root module

--- caterva2/services/dirroot.py
+++ caterva2/services/dirroot.py
@@ -1,3 +1,12 @@
+###############################################################################
+# Caterva2 - On demand access to remote Blosc2 data repositories
+#
+# Copyright (c) 2023 The Blosc Developers <blosc@blosc.org>
+# https://www.blosc.org
+# License: GNU Affero General Public License v3.0
+# See LICENSE.txt for details about copyright and rights to use.
+###############################################################################
+
 import io
 import os
 import pathlib
