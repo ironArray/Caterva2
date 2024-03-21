20c7f54c9cff2621537c84fc9e3c710f0e2be8a3
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2024-03-21 13:21:28 +0100

    Tests: Test for supported HDF5 datasets not marked as unsupported

--- caterva2/tests/test_hdf5root.py
+++ caterva2/tests/test_hdf5root.py
@@ -9,10 +9,25 @@
 
 import pytest
 
+import caterva2 as cat2
+
+from .services import TEST_HDF5_ROOT
+
 
 hdf5root = pytest.importorskip('caterva2.services.hdf5root',
                                reason="HDF5 support not present")
 
 
-def test_test(services):
-    pass
+@pytest.fixture
+def sub_host(services):
+    return services.get_endpoint('subscriber')
+
+
+@pytest.fixture
+def api_root(sub_host):
+    return cat2.Root(TEST_HDF5_ROOT, host=sub_host)
+
+
+def test_not_unsupported(api_root):
+    for node in api_root.node_list:
+        assert not node.startswith('unsupported/')
