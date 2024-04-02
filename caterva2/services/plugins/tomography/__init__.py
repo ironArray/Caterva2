1480f693a253681d01fdb61db688cce4af58b9fd
J. David Ibáñez <jdavid.ibp@gmail.com>
2024-04-02 14:08:16 +0200

    tomography: display thumbnail
    Add link to open full image in new tab.

--- caterva2/services/plugins/tomography/__init__.py
+++ caterva2/services/plugins/tomography/__init__.py
@@ -14,15 +14,15 @@ app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-name = 'tomography' # Identifies the plugin
-contenttype = 'tomography'
+name = "tomography" # Identifies the plugin
+contenttype = "tomography"
 
 
 cache = None
 partial_download = None
 
 def init(sub_cache, f):
-    print('INIT', sub_cache)
+    #print("INIT", sub_cache)
     global cache
     cache = sub_cache
     global partial_download
@@ -38,13 +38,52 @@ def display(
     abspath = srv_utils.cache_lookup(cache, cache / path)
     arr = blosc2.open(abspath)
 
+    base = f"/plugins/{name}"
     context = {
-        "name": name,
-        "path": path,
+        "href": f"{base}/display_one/{path}",
+        "pages": arr.shape[0],
     }
-    context['pages'] = arr.shape[0]
-    response = templates.TemplateResponse(request, "display.html", context=context)
-    return response
+    return templates.TemplateResponse(request, "display.html", context=context)
+
+
+async def __get_image(path, i):
+    from PIL import Image
+
+    abspath = srv_utils.cache_lookup(cache, cache / path)
+    await partial_download(abspath, str(path))
+    arr = blosc2.open(abspath)
+
+    img = arr[i,:]
+    return Image.fromarray(img)
+
+
+@app.get("/display_one/{path:path}", response_class=HTMLResponse)
+async def display_one(
+    request: Request,
+    # Path parameters
+    path: pathlib.Path,
+    # Query parameters
+    i: int,
+):
+
+    img = await __get_image(path, i)
+    width = 768  # Max size
+
+    base = f"/plugins/{name}"
+
+    links = []
+    if width and img.width > width:
+        links.append({
+            "href": f"{base}/image/{path}?i={i}",
+            "label": f"{img.width} x {img.height} (orignal size)",
+            "target": "blank_",
+        })
+
+    context = {
+        "src": f"{base}/image/{path}?{i=}&{width=}",
+        "links": links
+    }
+    return templates.TemplateResponse(request, "display_one.html", context=context)
 
 
 @app.get("/image/{path:path}")
@@ -53,22 +92,21 @@ async def image_file(
     # Path parameters
     path: pathlib.Path,
     # Query parameters
-    i: int
+    i: int,
+    width: int | None = None,
 ):
-    from PIL import Image
 
-    abspath = srv_utils.cache_lookup(cache, cache / path)
-    await partial_download(abspath, str(path))
-    arr = blosc2.open(abspath)
+    img = await __get_image(path, i)
 
-    img = arr[i,:]
-    img = Image.fromarray(img)
+    if width and img.width > width:
+        height = (img.height * width) // img.width
+        img = img.resize((width, height))
 
     img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
+    img.save(img_byte_arr, format="PNG")
     img_byte_arr.seek(0)
 
     def iterfile():
         yield from img_byte_arr
 
-    return responses.StreamingResponse(iterfile(), media_type='image/png')
+    return responses.StreamingResponse(iterfile(), media_type="image/png")
