1b70099f0d1be971a9591a6b28e37177837aa729
J. David Ibáñez <jdavid.ibp@gmail.com>
2024-09-24 13:36:28 +0200

    Fix create-user in pyproject

--- caterva2/tools/create_user.py
+++ caterva2/tools/create_user.py
@@ -1,7 +1,7 @@
 from caterva2 import utils
 
 
-if __name__ == '__main__':
+def main():
     conf = utils.get_conf('subscriber', allow_id=True)
     _stdir = '_caterva2/sub' + (f'.{conf.id}' if conf.id else '')
     parser = utils.get_parser(statedir=conf.get('.statedir', _stdir), id=conf.id)
@@ -11,3 +11,7 @@ if __name__ == '__main__':
     statedir = args.statedir.resolve()
     user = utils.create_user(args.username, state_dir=statedir)
     print('Password:', user.password)
+
+
+if __name__ == '__main__':
+    main()
