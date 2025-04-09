4f8383d7beefbd496c684015cf2ef127154c0a32
Francesc Alted <francesc@blosc.org>
2025-04-09 06:33:19 +0200

    Fix the example

--- examples/query_subscriber.py
+++ examples/query_subscriber.py
@@ -6,41 +6,35 @@
 # License: GNU Affero General Public License v3.0
 # See LICENSE.txt for details about copyright and rights to use.
 ###############################################################################
-import os
 from time import time
 
-# Small example of how to query a dataset on a subscriber
-
-import httpx
 import blosc2
+
+# Small example of how to query a dataset on a subscriber
+import httpx
 import numpy as np
 
 import caterva2 as cat2
 
-
 # Use the demo server
-URLBASE = 'https://demo.caterva2.net/'
-ROOT_NAME = 'example'
+URLBASE = "https://demo.caterva2.net/"
+ROOT_NAME = "example"
 
 user_auth = None
 # Uncomment the following line and use your username and password
 # if the subscriber requires authentication.
-#user_auth = {'username': 'user@example.com', 'password': 'foobar11'}
+# user_auth = {'username': 'user@example.com', 'password': 'foobar11'}
 
-auth_cookie = (cat2.api_utils.get_auth_cookie(URLBASE, user_auth)
-               if user_auth else None)
-
+client = cat2.Client(URLBASE, auth=user_auth)
 # Get the list of available roots
-roots = cat2.get_roots(URLBASE, auth_cookie=auth_cookie)
+roots = client.get_roots()
 print(roots[ROOT_NAME])
-# Subscribe to a root
-response = cat2.subscribe(ROOT_NAME, URLBASE, auth_cookie=auth_cookie)
 # Get a handle to the root
-example = cat2.Root(ROOT_NAME, urlbase=URLBASE, user_auth=user_auth)
+example = client.get(ROOT_NAME)
 # List the datasets in that root
 print(example.file_list)
 # Get a specific dataset
-array = example['dir1/ds-2d.b2nd']
+array = example["dir1/ds-2d.b2nd"]
 print(array.name, array.path)
 # Get some data out of the dataset
 print(array[:2])
@@ -49,23 +43,18 @@ print(array[:2])
 # 1. Direct download
 t0 = time()
 urlpath = array.get_download_url()
-data = httpx.get(urlpath, headers=({'Cookie': auth_cookie} if auth_cookie else None))
+data = httpx.get(urlpath, auth=user_auth)
 t = time() - t0
 # print(urlpath)
 print(f"Time for downloading data (HTTP): {t:.3f}s - {len(data.content) / 2**10:.2f} KB")
 mem_array = blosc2.ndarray_from_cframe(data.content)
-# print(mem_array.info)
+print(mem_array.info)
 
-# 2. Download to a local file
+# 2. Fetch the data from the subscriber
 t0 = time()
-localpath = array.download()
+mem_array2 = array[:]
 t = time() - t0
-print(f"Time for downloading data (API) : {t:.3f}s - {os.path.getsize(localpath) / 2**10:.2f} KB")
-data2 = open(localpath, 'rb').read()
-disk_array = blosc2.open(localpath)
+print(f"Time for downloading data (API) : {t:.3f}s - {mem_array2.nbytes / 2**10:.2f} KB")
 
 # Check that both methods return the same data
-np.testing.assert_array_equal(disk_array[:], mem_array[:])
-
-# Get some info on local (downloaded) data
-print(disk_array.info)
+np.testing.assert_array_equal(mem_array2, mem_array[:])
